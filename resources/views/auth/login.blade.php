3ec909f205aee86cacf6b001f16b8df467fd562a
AlessioAbbati <alex_117@live.it>
2023-07-06 11:01:23 +0200

    fix register and web

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
 
     <div class="mb-3">
       <label for="email" class="form-label">Email address</label>
-      <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" :value="old('email')" required autofocus autocomplete="username">
+      <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" value="old('email')" required autofocus autocomplete="username">
     </div>
     <div class="mb-3">
       <label for="password" class="form-label">Password</label>
--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,19 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guests\PageController as GuestsPageController;
+use App\Http\Controllers\Admin\PageController as AdminPageController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [GuestsPageController::class, 'home'])->name('guests.home');
 
-Route::middleware('auth')->group(function () {
+Route::get('/admin', [AdminPageController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('admin.dashboard');
+
+Route::middleware('auth')
+->name('admin.')
+->prefix('admin')
+->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
